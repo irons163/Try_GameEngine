f8a8f4936b6145d88d4dc93b310ff6ef1242534a
Phil <wen-chuan.chang@senao.com>
2017-01-26 17:56:15 +0800

    Add extension_nodule:Skill.

--- Try_GameEngine5/gen/com/example/try_gameengine/R.java
+++ Try_GameEngine5/gen/com/example/try_gameengine/R.java
@@ -17,37 +17,37 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static int activity_horizontal_margin=0x7f040000;
-        public static int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static int black_point=0x7f020000;
-        public static int blue_point=0x7f020001;
-        public static int brown_point=0x7f020002;
-        public static int down_keyboard_btn=0x7f020003;
-        public static int green_point=0x7f020004;
-        public static int ic_launcher=0x7f020005;
-        public static int left_keyboard_btn=0x7f020006;
-        public static int orange_point=0x7f020007;
-        public static int red_point=0x7f020008;
-        public static int right_keyboard_btn=0x7f020009;
-        public static int up_keyboard_btn=0x7f02000a;
-        public static int white_point=0x7f02000b;
-        public static int yellow_point=0x7f02000c;
+        public static final int black_point=0x7f020000;
+        public static final int blue_point=0x7f020001;
+        public static final int brown_point=0x7f020002;
+        public static final int down_keyboard_btn=0x7f020003;
+        public static final int green_point=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int left_keyboard_btn=0x7f020006;
+        public static final int orange_point=0x7f020007;
+        public static final int red_point=0x7f020008;
+        public static final int right_keyboard_btn=0x7f020009;
+        public static final int up_keyboard_btn=0x7f02000a;
+        public static final int white_point=0x7f02000b;
+        public static final int yellow_point=0x7f02000c;
     }
     public static final class id {
-        public static int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080000;
     }
     public static final class layout {
-        public static int activity_main=0x7f030000;
+        public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static int main=0x7f070000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static int action_settings=0x7f050001;
-        public static int app_name=0x7f050000;
-        public static int hello_world=0x7f050002;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
@@ -71,10 +71,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060001;
     }
 }
